4d5cbaea3f33f40411c8c99dba1a073dda12b371
Mudox <mudoxisme@gmail.com>
2015-08-23 00:27:58 +0800

    run fzf-cmdhub.py with installing it to .bin

--- fzf-cmdhub.sh
+++ fzf-cmdhub.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+FZF_CMDHUB_SH_PATH="$(dirname $0)"
+
 ch() {
   if [ -z "$(which fzf)" ]; then
     printf "\e[31m* need [fzf], please install it first *\e[0m\n"
     return
   fi
 
-  local selected_title=$(fzf-cmdhub-py -t | fzf)
+  local py_path="${FZF_CMDHUB_SH_PATH}/fzf-cmdhub.py"
+
+  local selected_title="$(python ${py_path} -t | fzf)"
   if [ -n "$selected_title" ]; then
-    local cmd=$(fzf-cmdhub-py -c "${selected_title}")
+    local cmd="$(python ${py_path} -c ${selected_title})"
     if [ -n "$cmd" ]; then
       eval "$cmd"
     fi
   fi
 }
 
-FZF_CMDHUB_SH_PATH=$0
-
-ct() {
-  echo "$FZF_CMDHUB_SH_PATH"
-}
