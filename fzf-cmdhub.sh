cc6e6d1be4d8cf0123cb7637b3d7a8fb28ae8f3a
mudox <mudoxisme@gmail.com>
2015-10-29 20:12:43 +0800

    change fzf options

--- fzf-cmdhub.sh
+++ fzf-cmdhub.sh
@@ -9,7 +9,8 @@ ch() {
 
   local py_path="${FZF_CMDHUB_SH_PATH}/fzf-cmdhub.py"
 
-  local selected_title="$(python ${py_path} -t | fzf --bind='ctrl-e:execute(env MDX_CHAMELEON_MODE=mini nvim ~/.fzf-cmdhub-menu)')"
+  local selected_title="$(python ${py_path} -t \
+    | fzf -e --bind='ctrl-e:execute(env MDX_CHAMELEON_MODE=mini nvim ~/.fzf-cmdhub-menu)')"
 
   if [ -n "$selected_title" ]; then
     local cmd="$(python ${py_path} -c ${selected_title})"
