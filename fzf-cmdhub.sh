23f3b637611b751d46d79dfd343ba54204b8432d
mudox <mudoxisme@gmail.com>
2016-03-06 11:33:05 +0800

    fix shellcheck issues.

--- fzf-cmdhub.sh
+++ fzf-cmdhub.sh
@@ -1,7 +1,9 @@
-FZF_CMDHUB_SH_PATH="$(dirname $0)"
+#!/usr/bin/env bash
+
+FZF_CMDHUB_SH_PATH=$(dirname "$0")
 
 __fzfcmd() {
-  [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" \
+  [ "${FZF_TMUX:-1}" -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" \
     || echo "fzf"
 }
 
@@ -14,7 +16,9 @@ ch() {
 
   local py_path="${FZF_CMDHUB_SH_PATH}/fzf-cmdhub.py"
 
-  local ret="$(python ${py_path} -t \
+  # TODO!: refactor the ctrl-e behavior to edit the selected item whatever it is.
+  local ret
+  ret=$(python "${py_path}" -t \
     | $(__fzfcmd) \
     --exact \
     --header='Tip: press ctrl-e to edit this menu' \
@@ -27,10 +31,11 @@ ch() {
   elif [[ "$ret" == '' ]]; then
     return
   else
-    local cmd="$(python ${py_path} -c ${ret#*$'\n'})"
-    if [ -n "$cmd" ]; then
-      printf "\e[35mexecuting: \e[34m$cmd\e[0m\n"
-      eval $cmd
+    local cmd
+    cmd=$(python "${py_path}" -c "${ret#*$'\n'}")
+    if [[ -n "$cmd" ]]; then
+      printf "\e[35mexecuting: \e[34m%s\e[0m\n" "$cmd"
+      eval "$cmd"
     else
       printf "\e[31mfechted an empty content\e[0m\n"
     fi
